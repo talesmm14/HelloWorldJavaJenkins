a2409ea5dc225a85290fee74e758b1caa4d07f94
Tales Melquiades <talesmelquiades@hotmail.com>
2019-08-29 09:13:46 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String args[]){
 		int a = 2, b = 4;
 		System.out.println("Hello World, Jenkins!!!");
-		System.out.println(a + b)
+		System.out.println(a + b);
 
 	}
 
